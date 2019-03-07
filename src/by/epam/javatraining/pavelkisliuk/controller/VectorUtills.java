f1e0fac89017699aacad243ec6f30b0e7a6659a2
PavelKisliuk <pavelsergeevichkisliuk2015@mail.ru>
2019-03-07 15:32:52 +0300

    add increase and decrease specification for merge and insertion sort

--- src/by/epam/javatraining/pavelkisliuk/controller/VectorUtills.java
+++ src/by/epam/javatraining/pavelkisliuk/controller/VectorUtills.java
@@ -4,71 +4,64 @@ import by.epam.javatraining.pavelkisliuk.model.Vector;
 
 public class VectorUtills {
 
-	public static double max(Vector N)
-	{
+	public static double max(Vector N) {
 		double max = N.getElement(0);
-		for(int i = 1; i < N.size(); i++) {
-			if(Double.compare(max, N.getElement(i)) < 0) {
+		for (int i = 1; i < N.size(); i++) {
+			if (Double.compare(max, N.getElement(i)) < 0) {
 				max = N.getElement(i);
 			}
 		}
 		return max;
 	}
 
-	public static double min(Vector N)
-	{
+	public static double min(Vector N) {
 		double min = N.getElement(0);
-		for(int i = 1; i < N.size(); i++) {
-			if(Double.compare(min, N.getElement(i)) > 0) {
+		for (int i = 1; i < N.size(); i++) {
+			if (Double.compare(min, N.getElement(i)) > 0) {
 				min = N.getElement(i);
 			}
 		}
 		return min;
 	}
 
-	public static double average(Vector N)
-	{
+	public static double average(Vector N) {
 		double average = 0.0;
-		for(int i = 0; i < N.size(); i++) {
+		for (int i = 0; i < N.size(); i++) {
 			average += N.getElement(i);
 		}
 		return (average / N.size());
 	}
 
-	public static boolean isSortedIncrease(Vector N)
-	{
-		for(int i = 0; i < (N.size() - 1); i++) {
-			if(Double.compare(N.getElement(i), N.getElement(i + 1)) > 0) {
+	public static boolean isSortedIncrease(Vector N) {
+		for (int i = 0; i < (N.size() - 1); i++) {
+			if (Double.compare(N.getElement(i), N.getElement(i + 1)) > 0) {
 				return false;
 			}
 		}
 		return true;
 	}
 
-	public static boolean isSortedDecrease(Vector N)
-	{
-		for(int i = 0; i < (N.size() - 1); i++) {
-			if(Double.compare(N.getElement(i), N.getElement(i + 1)) < 0) {
+	public static boolean isSortedDecrease(Vector N) {
+		for (int i = 0; i < (N.size() - 1); i++) {
+			if (Double.compare(N.getElement(i), N.getElement(i + 1)) < 0) {
 				return false;
 			}
 		}
 		return true;
 	}
 
-	public static void reverse(Vector N)
-	{
-		for(int starting = 0, ending = (N.size() - 1); starting != (N.size() / 2); starting++, ending--) {
+	public static void reverse(Vector N) {
+		for (int starting = 0, ending = (N.size() - 1); starting != (N.size() / 2); starting++, ending--) {
 			double temp = N.getElement(starting);
 			N.setElement(N.getElement(ending), starting);
 			N.setElement(temp, ending);
 		}
 	}
 
-	public static void bubbleSortIncrease(Vector N)
-	{
-		for(int pass = 0; pass < N.size(); pass++) {
-			for(int i = 0; i < (N.size() - 1); i++) {
-				if(Double.compare(N.getElement(i), N.getElement(i + 1)) > 0) {
+	public static void bubbleSortIncrease(Vector N) {
+		for (int pass = 0; pass < N.size(); pass++) {
+			for (int i = 0; i < (N.size() - 1); i++) {
+				if (Double.compare(N.getElement(i), N.getElement(i + 1)) > 0) {
 					double temp = N.getElement(i);
 					N.setElement(N.getElement(i + 1), i);
 					N.setElement(temp, i + 1);
@@ -77,11 +70,10 @@ public class VectorUtills {
 		}
 	}
 
-	public static void bubbleSortDecrease(Vector N)
-	{
-		for(int pass = 0; pass < N.size(); pass++) {
-			for(int i = 0; i < (N.size() - 1); i++) {
-				if(Double.compare(N.getElement(i), N.getElement(i + 1)) < 0) {
+	public static void bubbleSortDecrease(Vector N) {
+		for (int pass = 0; pass < N.size(); pass++) {
+			for (int i = 0; i < (N.size() - 1); i++) {
+				if (Double.compare(N.getElement(i), N.getElement(i + 1)) < 0) {
 					double temp = N.getElement(i);
 					N.setElement(N.getElement(i + 1), i);
 					N.setElement(temp, i + 1);
@@ -90,53 +82,50 @@ public class VectorUtills {
 		}
 	}
 
-	public static void enhancedBubbleSortIncrease(Vector N)
-	{
+	public static void enhancedBubbleSortIncrease(Vector N) {
 		int passCounter = N.size();
-		for(int pass = 0; pass < N.size(); pass++) {
+		for (int pass = 0; pass < N.size(); pass++) {
 			passCounter--;
 			boolean exchangeFlag = false;
-			for(int i = 0; i < passCounter; i++) {
-				if(Double.compare(N.getElement(i), N.getElement(i + 1)) > 0) {
+			for (int i = 0; i < passCounter; i++) {
+				if (Double.compare(N.getElement(i), N.getElement(i + 1)) > 0) {
 					double temp = N.getElement(i);
 					N.setElement(N.getElement(i + 1), i);
 					N.setElement(temp, i + 1);
 					exchangeFlag = true;
 				}
 			}
-			if(!exchangeFlag) {
+			if (!exchangeFlag) {
 				break;
 			}
 		}
 	}
 
-	public static void enhancedBubbleSortDecrease(Vector N)
-	{
+	public static void enhancedBubbleSortDecrease(Vector N) {
 		int passCounter = N.size();
-		for(int pass = 0; pass < N.size(); pass++) {
+		for (int pass = 0; pass < N.size(); pass++) {
 			passCounter--;
 			boolean exchangeFlag = false;
-			for(int i = 0; i < passCounter; i++) {
-				if(Double.compare(N.getElement(i), N.getElement((i + 1))) < 0) {
+			for (int i = 0; i < passCounter; i++) {
+				if (Double.compare(N.getElement(i), N.getElement((i + 1))) < 0) {
 					double temp = N.getElement(i);
 					N.setElement(N.getElement((i + 1)), i);
 					N.setElement(temp, (i + 1));
 					exchangeFlag = true;
 				}
 			}
-			if(!exchangeFlag) {
+			if (!exchangeFlag) {
 				break;
 			}
 		}
 	}
 
-	public static void selectionSortIncrease(Vector N)
-	{
-		for(int i = 0; i < N.size(); i++) {
+	public static void selectionSortIncrease(Vector N) {
+		for (int i = 0; i < N.size(); i++) {
 			int minElementIndex = i;
 			double min = N.getElement(i);
-			for(int j = i; j < N.size(); j++) {
-				if(Double.compare(min, N.getElement(j)) > 0) {
+			for (int j = i; j < N.size(); j++) {
+				if (Double.compare(min, N.getElement(j)) > 0) {
 					min = N.getElement(j);
 					minElementIndex = j;
 				}
@@ -146,13 +135,12 @@ public class VectorUtills {
 		}
 	}
 
-	public static void selectionSortDecrease(Vector N)
-	{
-		for(int i = 0; i < N.size(); i++) {
+	public static void selectionSortDecrease(Vector N) {
+		for (int i = 0; i < N.size(); i++) {
 			int maxElementIndex = i;
 			double max = N.getElement(i);
-			for(int j = i; j < N.size(); j++) {
-				if(Double.compare(max, N.getElement(j)) < 0) {
+			for (int j = i; j < N.size(); j++) {
+				if (Double.compare(max, N.getElement(j)) < 0) {
 					max = N.getElement(j);
 					maxElementIndex = j;
 				}
@@ -162,10 +150,9 @@ public class VectorUtills {
 		}
 	}
 
-	public static void insertionSort(Vector N)
-	{
-		for(int i = 1; i < N.size(); i++) {
-			for(int j = i; (j > 0) && (Double.compare(N.getElement((j - 1)), N.getElement(j)) > 0); j--) {
+	public static void insertionSortIncrease(Vector N) {
+		for (int i = 1; i < N.size(); i++) {
+			for (int j = i; (j > 0) && (Double.compare(N.getElement((j - 1)), N.getElement(j)) > 0); j--) {
 				double temp = N.getElement((j - 1));
 				N.setElement(N.getElement(j), (j - 1));
 				N.setElement(temp, j);
@@ -173,33 +160,39 @@ public class VectorUtills {
 		}
 	}
 
-	public static void mergeSort(Vector N)
-	{
-		VectorUtills.mergeSort(N, (0), (N.size() - 1));
-	}
-
-	private static void mergeSort(Vector N, int startIndex, int endIndex)
-	{
-		if (startIndex < endIndex)
-		{
-			mergeSort(N, startIndex, ((startIndex + endIndex) / 2));
-			mergeSort(N, (((startIndex + endIndex) / 2) + 1), endIndex);
-			VectorUtills.merge(N, startIndex, endIndex);
+	public static void insertionSortDecrease(Vector N) {
+		for (int i = 1; i < N.size(); i++) {
+			for (int j = i; (j > 0) && (Double.compare(N.getElement((j - 1)), N.getElement(j)) < 0); j--) {
+				double temp = N.getElement((j - 1));
+				N.setElement(N.getElement(j), (j - 1));
+				N.setElement(temp, j);
+			}
 		}
 	}
 
-	private static void merge(Vector N, int startIndex, int endIndex)
-	{
+	public static void mergeSortIncrease(Vector N) {
+		VectorUtills.mergeSortIncrease(N, (0), (N.size() - 1));
+	}
+
+	private static void mergeSortIncrease(Vector N, int startIndex, int endIndex) {
+		if (startIndex < endIndex) {
+			VectorUtills.mergeSortIncrease(N, startIndex, ((startIndex + endIndex) / 2));
+			VectorUtills.mergeSortIncrease(N, (((startIndex + endIndex) / 2) + 1), endIndex);
+			VectorUtills.mergeIncrease(N, startIndex, endIndex);
+		}
+	}
+
+	private static void mergeIncrease(Vector N, int startIndex, int endIndex) {
 		Vector tempVector = new Vector(N);
 		int middle = (startIndex + endIndex) / 2;
 		int start = startIndex;
 		int end = middle + 1;
-		for(int i = startIndex; i <= endIndex; i++) {
-			if ((start <= middle) && ((end > endIndex) || (Double.compare(N.getElement(start), N.getElement(end)) < 0))) {
+		for (int i = startIndex; i <= endIndex; i++) {
+			if ((start <= middle) && ((end > endIndex) ||
+					(Double.compare(N.getElement(start), N.getElement(end)) < 0))) {
 				tempVector.setElement(N.getElement(start), i);
 				start++;
-			}
-			else {
+			} else {
 				tempVector.setElement(N.getElement(end), i);
 				end++;
 			}
@@ -208,5 +201,48 @@ public class VectorUtills {
 		for (int i = startIndex; i <= endIndex; i++) {
 			N.setElement(tempVector.getElement(i), i);
 		}
+	}
+
+	public static void mergeSortDecrease(Vector N) {
+		VectorUtills.mergeSortDecrease(N, (0), (N.size() - 1));
+	}
+
+	private static void mergeSortDecrease(Vector N, int startIndex, int endIndex) {
+		if (startIndex < endIndex) {
+			VectorUtills.mergeSortDecrease(N, startIndex, ((startIndex + endIndex) / 2));
+			VectorUtills.mergeSortDecrease(N, (((startIndex + endIndex) / 2) + 1), endIndex);
+			VectorUtills.mergeDecrease(N, startIndex, endIndex);
+		}
+	}
+
+	private static void mergeDecrease(Vector N, int startIndex, int endIndex) {
+		Vector tempVector = new Vector(N);
+		int middle = (startIndex + endIndex) / 2;
+		int start = startIndex;
+		int end = middle + 1;
+		for (int i = startIndex; i <= endIndex; i++) {
+			if ((start <= middle) && ((end > endIndex) ||
+					(Double.compare(N.getElement(start), N.getElement(end)) > 0))) {
+				tempVector.setElement(N.getElement(start), i);
+				start++;
+			} else {
+				tempVector.setElement(N.getElement(end), i);
+				end++;
+			}
+		}
+
+		for (int i = startIndex; i <= endIndex; i++) {
+			N.setElement(tempVector.getElement(i), i);
+		}
+	}
+
+	public static void quickSortIncrease(Vector N)
+	{
+
+	}
+
+	public static void quickSortIncrease(Vector N)
+	{
+
 	}
 }
